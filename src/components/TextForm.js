87e161cae84680d8acf32d4e05e3aa53c982ff08
areeba <areebaadeen15@gmail.com>
2024-07-19 21:32:05 +0500

    my first react app

---
+++ src/components/TextForm.js
@@ -0,0 +1,54 @@
+import React, {useState} from 'react'
+import { copy } from 'clipboard'
+
+export default function TextForm(props) {
+    const handleUpClick = ()=>{
+        //console.log("Uppercase was clicked" + text);
+        let newText = text.toUpperCase();
+        setText(newText)
+    }
+    const handleLoClick = ()=>{
+        //console.log("Uppercase was clicked" + text);
+        let newText = text.toLowerCase();
+        setText(newText)
+    }
+    const handleClClick = ()=>{
+        let newText = '';
+        setText(newText)
+    }
+    const handleCoClick = ()=>{
+        copy(text);
+        alert(
+            "Text copied!"
+        )
+    }
+    const handleOnChange = (event)=>{
+        //console.log("On Change");
+        setText(event.target.value);
+    }
+
+    const[text, setText] = useState('');
+
+  return (
+    <>
+    <div className="container">
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+            <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+        </div>
+        <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
+        <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to Lowercase</button>
+        <button className="btn btn-primary mx-2" onClick={handleClClick}>Clear Text</button>
+        <button className="btn btn-primary mx-2" onClick={handleCoClick}>Copy Text</button>
+
+    </div>
+    <div className="container my-3">
+        <h2>Your text summary</h2>
+        <p>{text.split(" ").length} words and {text.length} characters</p>
+        <p>{0.008 *  text.split(" ").length} Minutes read</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+    </div>
+    </>
+  )
+}
